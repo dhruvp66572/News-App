fcdf97f12851fb6fc4ad2e042feb07efb29ccbcc
DHRUV PRAJAPATI <dhruvprajapati66572@gmail.com>
2023-10-30 00:19:27 +0530

    News App Added

---
+++ src/components/News.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export default class News extends Component {
+  render() {
+    return (
+      <div>This is a News Base Component
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+      </div>
+
+    )
+  }
+}
