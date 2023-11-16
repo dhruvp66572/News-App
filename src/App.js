1ba7dcbc6cfed324cb593a58aa80883c08aaed87
DHRUV PRAJAPATI <dhruvprajapati66572@gmail.com>
2023-11-17 01:14:08 +0530

    Simple App Uses Next And Previous Btn

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import {
 
 export default class App extends Component {
   render() {
+    this.pageSize = 9
     return (
       <>
         <Router>
@@ -20,28 +21,29 @@ export default class App extends Component {
             <NavBar />
             <Switch>
               <Route exact path="/" >
-                <News key={"general"} pageSize={6} country="in" category="general"/>
+                <News key={"general"} pageSize={this.pageSize}country="in" category="general"/>
               </Route>
               <Route exact path="/business" >
-                <News key={"business"} pageSize={6} country="in" category="business"  />
+                <News key={"business"} pageSize={this.pageSize} country="in" category="business"  />
+                News.forceUpdate();
               </Route>
               <Route exact path="/entertainment" >
-                <News key={"entertainment"} pageSize={6} country="in" category="entertainment"  />              
+                <News key={"entertainment"} pageSize={this.pageSize} country="in" category="entertainment"  />              
               </Route>
               <Route exact path="/general" >
-                <News key={"general"} pageSize={6} country="in" category="general"/>
+                <News key={"general"} pageSize={this.pageSize} country="in" category="general"/>
               </Route>
               <Route exact path="/health" >
-                <News key={"health"} pageSize={6} country="in" category="health"  />
+                <News key={"health"} pageSize={this.pageSize} country="in" category="health"  />
               </Route>
               <Route exact path="/science" >
-                <News key={"science"} pageSize={6} country="in" category="science" />
+                <News key={"science"} pageSize={this.pageSize} country="in" category="science" />
               </Route>
               <Route exact path="/sports" >
-                <News key={"sports"} pageSize={6} country="in" category="sports"  />
+                <News key={"sports"} pageSize={this.pageSize} country="in" category="sports"  />
               </Route>
               <Route exact path="/technology" >
-                <News key={"technology"} pageSize={6} country="in" category="technology" />
+                <News key={"technology"} pageSize={this.pageSize} country="in" category="technology" />
               </Route>
             </Switch>
           </div>
