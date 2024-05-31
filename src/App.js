e1795a0d16d2362b632cc68afc5dbff2d2cad4ad
DHRUV PRAJAPATI <dhruvprajapati66572@gmail.com>
2024-05-31 20:28:26 +0530

    Some Issue Occur In API

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import News from "./components/News";
 import LoadingBar from "react-top-loading-bar";
 import { Route, Routes } from "react-router-dom";
 
+<<<<<<< HEAD
 const App = () => {
   const [progress, setProgress] = useState(0);
   const pageSize = 9;
@@ -134,3 +135,44 @@ const App = () => {
 };
 
 export default App;
+=======
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Router>
+          <div>
+            <NavBar />
+            <Switch>
+              <Route exact path="/" >
+                <News key={"general"} pageSize={6} country="in" category="general"/>
+              </Route>
+              <Route exact path="/business" >
+                <News key={"business"} pageSize={6} country="in" category="business"  />
+              </Route>
+              <Route exact path="/entertainment" >
+                <News key={"entertainment"} pageSize={6} country="in" category="entertainment"  />              
+              </Route>
+              <Route exact path="/general" >
+                <News key={"general"} pageSize={6} country="in" category="general"/>
+              </Route>
+              <Route exact path="/health" >
+                <News key={"health"} pageSize={6} country="in" category="health"  />
+              </Route>
+              <Route exact path="/science" >
+                <News key={"science"} pageSize={6} country="in" category="science" />
+              </Route>
+              <Route exact path="/sports" >
+                <News key={"sports"} pageSize={6} country="in" category="sports"  />
+              </Route>
+              <Route exact path="/technology" >
+                <News key={"technology"} pageSize={6} country="in" category="technology" />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </>
+    );
+  }
+}
+>>>>>>> parent of 1ba7dcb (Simple App Uses Next And Previous Btn)
